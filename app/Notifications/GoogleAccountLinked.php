bdf0cb6b4a32137f0c2636811d33a309e95b3ba1
Stanislav Serebrennikov <goodsmileduck@gmail.com>
2026-06-09 20:40:20 +0800

    feat(oauth): add google-account-linked notification

---
+++ app/Notifications/GoogleAccountLinked.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class GoogleAccountLinked extends Notification
+{
+    use Queueable;
+
+    /**
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject(__('A Google sign-in was connected to your account'))
+            ->line(__('A Google account was just linked to your :app account and can now be used to sign in.', ['app' => config('app.name')]))
+            ->line(__('If this was not you, please change your password and contact support immediately.'));
+    }
+}
